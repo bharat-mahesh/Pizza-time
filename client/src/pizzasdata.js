f8ce5ec039bc74bf751ecea71acb1567750dcd51
Shubham Mehta <smm1@somaiya.edu>
2022-11-21 13:47:25 +0530

    Shubham

---
+++ client/src/pizzasdata.js
@@ -0,0 +1,116 @@
+const pizzas =[
+    {
+        "name": "Gluten Free Vegan Pizza",
+        "varients": [
+        "small",
+        "medium",
+        "large"
+        ],
+        "prices": [
+            {
+            "small":220,
+            "medium":300,
+            "large":350
+            },
+        ],
+        "category":"veg",
+        "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBgmqzj7eikbrgVxSxK6DJFji4Lj8ewkn7Lw&usqp=CAU",
+        "description":" Gluten-free pizza is pizza that is not made from wheat flour and contains less than 20 ppm of gluten. 1. Besides wheat, other cereals containing gluten are rye, barley, triticale, and spelt. Gluten-free pizzas are developed for people who suffer from celiac disease."
+  },
+  {
+    "name": "Non Veg Trouble",
+    "varients":[
+      "small",
+      "medium",
+      "large"
+    ],
+    "prices":[
+     {
+       "small":320,
+       "medium":400,
+       "large":550
+     },
+    ],
+    "category":"non-veg",
+    "image": "https://5.imimg.com/data5/HS/NQ/MY-17758356/classic-pizzas-non-veg-trouble-014-500x500.png",
+    "description":"Naan topped with chicken tikka masala and mozzarella cheese makes this chicken tikka pizza a tasty Italian/Indian combination"
+  },{
+  
+  
+  
+    "name": "Margherita",
+    "varients":[
+      "small",
+      "medium",
+      "large"
+    ],
+    "prices":[
+     {
+       "small":120,
+       "medium":200,
+       "large":350
+     },
+    ],
+    "category":"veg",
+    "image":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3YswZDvXy9L7xR2tkA1T1JloeJU2FMhyQ5Q&usqp=CAU",
+    "description":"Pizza margherita, as the Italians call it, is a simple pizza hailing from Naples. When done right, margherita pizza features a bubbly crust, crushed San Marzano tomato sauce, fresh mozzarella and basil, a drizzle of olive oil, and a sprinkle of salt"
+  },
+  {
+  
+    "name": "Veg Supreme",
+    "varients":[
+      "small",
+      "medium",
+      "large"
+    ],
+    "prices":[
+     {
+       "small":320,
+       "medium":400,
+       "large":450
+     },
+    ],
+    "category":"veg",
+    "image":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSnckl1vZlEkD5TElKBcB2r3GigAOgqhDnx9g&usqp=CAU",
+    "description":"The veggie supreme pizza is a classic for a reason. Crispy golden crust, light and fresh tomato sauce, melty mozzarella, sweet bell peppers, zest onion, and sliced mushrooms are topped with oregano and basil for a pizza pie that can't be beat!"
+  },
+  {
+  
+    "name": "Chicken Mushroom",
+    "varients":[
+    "small",
+    "medium",
+    "large"
+    ],
+    "prices":[
+     {
+       "small":420,
+       "medium":500,
+       "large":550
+     },
+    ],
+    "category":"non-veg",
+    "image":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPQdzP_SYuFUYHt71BCUlfjapCGiSMdQjVvQ&usqp=CAU",
+    "description":" An Italian favorite, this chicken pizza recipe is a delicious mix of flat bread or base topped with cheese, chillies, onion, garlic sauce and chunks of chicken. Tossed to perfection, this quick and easy pizza recipe is the solution to all those chicken and cheese cravings.  "
+  },
+  {
+  
+    "name": "Paneer Tikka Pizza",
+    "varients":[
+      "small",
+      "medium",
+      "large"
+    ],
+    "prices":[
+     {
+       "small":340,
+       "medium":400,
+       "large":450
+     },
+    ],
+    "category":"non-veg",
+    "image":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyYStH-xnpu1s4QGOwbo0e2HWERFf0tLc-kQ&usqp=CAU",
+    "description":"The marinated paneer cubes with diced onions and capsicum is placed on pizza. once the paneer cubes are added, pizza base is topped with generous amount of mozzarella cheese and paneer pizza is ready."
+    },
+ ];
+export default pizzas
